530823935240ea31a284aa3d22a585538945731f
FaizaS <faiza_shahid12@hotmail.com>
2023-12-05 09:48:44 +0000

    archive_jenkins zip

--- run2.sh
+++ run2.sh
@@ -1,2 +1,9 @@
 echo 'Hello world2!'
 echo 'I created a webhook'
+
+echo 'This file is created by an echo 1' > 1_jenkin.txt
+echo 'This file is created by an echo 2' > 2_jenkin.txt
+echo 'This file is created by an echo 3' > 3_jenkin.txt
+echo 'This file is created by an echo 4' > 4_jenkin.txt
+
+zip archive_jenkins.zip *.txt
